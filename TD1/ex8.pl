253ea10ef3e108c79e1b83ac3673448e09731eb5
DA COSTA MOURA KEVIN <kevin.da-costa-moura@localhost.localdomain>
2022-01-20 16:18:08 +0100

    ex8&9 du TD1

--- TD1/ex8.pl
+++ TD1/ex8.pl
@@ -2,3 +2,20 @@
 use strict;
 use warnings;
 
+sub NbPremier {
+    my ($n) = @_;
+    my @lst = (2..$n);
+    my @res = ();
+    while ($#lst) {
+        my $val = $lst[0];
+        push(@res, $val);
+        shift(@lst);
+        @lst = grep {$_ % $val} @lst;
+    }
+    return @res;
+}
+
+
+
+my @res = NbPremier(100);
+print("@res\n");
--- TD1/ex9.pl
+++ TD1/ex9.pl
@@ -1,1 +1,21 @@
-#TODO
+#!user/bin/perl
+
+use strict;
+use warnings;
+
+sub Modif {
+    my ($texte, $ancien, $nouveau) = @_;
+    
+    my $res = 0;
+    my $i = 0;
+
+    while(($res = index($texte, $ancien, $i)) != -1) {
+        substr($texte, $res, length($ancien), $nouveau);
+        $i+= length($nouveau)
+    }
+
+    return ($texte);
+}
+
+print(Modif("bonjour vous, bonjour", "bonjour", "allo")."\n");
+print(Modif("bonjour vous, bonjour", "bonjour", "bonjour bonjour")."\n");
